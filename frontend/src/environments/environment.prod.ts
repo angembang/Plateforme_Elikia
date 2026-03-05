5630a143f0c78b488ea05378905c00dd119e9856
Ange <angembang@gmail.com>
2026-03-05 05:19:58 +0100

    ci: updated the backend application profile to prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: "https://www.association-elikia.fr"
+  apiUrl: "https://www.association-elikia.fr/api"
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: "https://www.association-elikia/api",
-  apiBackendUrl: "https://www.association-elikia:8080",
+  apiUrl: "https://www.association-elikia.fr/api",
+  apiBackendUrl: "https://www.association-elikia.fr",
   tinymceApiKey: "tezye03wmjsy94ybj3g8ccpeytbwk52p08mdwxp73zvujffe"
   // https:api.elikia.org
 };
