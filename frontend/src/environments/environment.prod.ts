2cd07c0945c7f898912d01af8ff2cdb965f0f6f0
Ange <angembang@gmail.com>
2026-03-10 00:43:59 +0100

    Fix media storage path

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: "https://www.association-elikia/api",
   apiBackendUrl: "https://www.association-elikia",
   tinymceApiKey: "tezye03wmjsy94ybj3g8ccpeytbwk52p08mdwxp73zvujffe"
