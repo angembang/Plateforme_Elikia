83166d6bf4b7e5a1e74fef03c90d4dd5e51257c8
iman_nazhat <emannzhat@gmail.com>
2026-06-18 00:27:38 +0200

    feat(inscriptions aux evenements):create event registration request dto

---
+++ backend/src/main/java/fr/elikia/backend/dto/EventRegistrationDTO.java
@@ -0,0 +1,43 @@
+package fr.elikia.backend.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class EventRegistrationDTO {
+    @NotBlank(message = "The first name is required")
+    private String firstName;
+
+    @NotBlank(message = "The last name is required")
+    private String lastName;
+
+    @NotBlank(message = "The email is required")
+    @Email(message = "The email format is invalid")
+    private String email;
+
+    public EventRegistrationDTO() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
