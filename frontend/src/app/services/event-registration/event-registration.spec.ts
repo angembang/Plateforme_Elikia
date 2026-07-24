34c657e5602f1bfdb003b1d71944549414128b08
iman_nazhat <emannzhat@gmail.com>
2026-07-24 16:20:14 +0200

    test: configure HttpClient providers for registration service tests

--- frontend/src/app/services/event-registration/event-registration.spec.ts
+++ frontend/src/app/services/event-registration/event-registration.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { EventRegistrationService } from './event-registration';
 
-describe('EventRegistration', () => {
+describe('EventRegistrationService', () => {
   let service: EventRegistrationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
+    });
+
     service = TestBed.inject(EventRegistrationService);
   });
 
